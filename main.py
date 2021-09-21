12e6af10bea1954eede15025e84d328005dd5784
244yam <gibson.60s.lp.std@gmail.com>
2021-09-22 08:39:06 +0900

    master

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ import base64
 
 st.header('アンチ選択くん')
 id1 = st.sidebar.text_input('遺伝子名','Gapdh')
-seq1 = st.sidebar.text_input('主配列を入力','GTCCGGATGCTGCGCCTGCGGTAGAGCGGCCGCCATGTTGCAACCGGGAAGGAAATGAATGGGCAGCCGTTAGGAAAGCCTGCCGGTGACTAACCCTGCGCTCCTGCCTCGATGGGTGGAGTCGCGTGTGGCGGGGAAGTCAGGTGGAGCGAGGCTAGCTGGCCC')
-seq2 = st.sidebar.text_input('参照配列1（Ref1）を入力','ATAGA')
-seq3 = st.sidebar.text_input('参照配列2（Ref2）を入力','ATATAGATAGAGATAGA')
+seq1 = st.sidebar.text_input('主配列を入力（例：AAATGGT...）','')
+seq2 = st.sidebar.text_input('比較配列1（Ref1）を入力（例：AAATGGT...）','')
+seq3 = st.sidebar.text_input('比較配列2（Ref2）を入力（例：AAATGGT...）','')
 
 seq1 = Seq(seq1.upper())
 seq2 = Seq(seq2.upper())
 seq3 = Seq(seq3.upper())
-numr = st.sidebar.slider('右翼の数', 0, 5,3)
-gap = st.sidebar.slider('右翼の数',0,15,8)
-numl = st.sidebar.slider('左翼の数',0,5,3)
+numr = st.sidebar.slider('左翼の数', 0, 5,3)
+gap = st.sidebar.slider('ギャップの数',0,15,8)
+numl = st.sidebar.slider('右翼の数',0,5,3)
 tail = st.sidebar.slider('テイルの数',0,5,0)
 
 
@@ -28,9 +28,9 @@ seq2r=str(seq2.reverse_complement())
 seq3r=str(seq3.reverse_complement())
 
 #emptyリスト
-list_df = pd.DataFrame( columns=['ID','右翼','ASO全体','左翼','GC%','cpg in gap','hom vs ref1', 'hom vs ref2'] )
+list_df = pd.DataFrame( columns=['ID','左翼','ASO全体','右翼','GC%','cpg in gap','hom vs ref1', 'hom vs ref2'] )
 #リスト追加（Seqクラスはstrに直してから使う）
-for i in range(len(seq1)-(int(numr)+int(gap)+int(numl)+int(tail)+1)):
+for i in range(len(seq1)-(int(numr)+int(gap)+int(numl)+int(tail))+1):
     tmp_se = pd.Series( 
     [id1+"-"+str(i+1)+"-ASO" ,  seq1r[i:i+int(numr)],
     seq1r[i:i+int(numr)]
