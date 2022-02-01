0aa4a8dcd32da870eae42dfba4923729a5794152
244yam <91118318+244yam@users.noreply.github.com>
2022-02-01 16:51:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd
 from Bio.Seq import Seq
 from Bio.SeqUtils import GC
 import base64
-import RNA
+import viennarna
 
 st.header('アンチ選択くん')
 id1 = st.sidebar.text_input('遺伝子名','Gapdh')
