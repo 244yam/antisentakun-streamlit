dbcde638e074e728b12b21bd67950faaa15f4765
244yam <91118318+244yam@users.noreply.github.com>
2023-03-13 00:32:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ if uploaded_file is not None:
     predf = pd.read_csv(uploaded_file)
     
     for i in len(predf):
-        predf['dG'] = dg(predf.iat[i,0], temp =37.0)
+        predf['dG'] = dg(str(predf.iat[i,0]), temp =37.0)
         st.write(predf)
         
     
