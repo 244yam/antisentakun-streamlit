bd92a716476b6d8307c821f332eed5913a93d479
244yam <91118318+244yam@users.noreply.github.com>
2023-03-16 00:23:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ x = list_tf.loc[:, 'atg':'CGT']
 with open('yoshidamodel.pkl', 'rb') as f:
     ktg = pickle.load(f)
     pred = ktg.predict(x)
-    tox = pd.DataFrame({'tox':pred})
+tox = pd.DataFrame({'tox':pred})
 fdf2 = fdf.join(tox)
 
 #=========================機械学習用===========ここまで
