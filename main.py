7f599b5841d4f045ccd96ce3fc6128222ad6fe41
244yam <91118318+244yam@users.noreply.github.com>
2023-03-16 00:30:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -91,7 +91,8 @@ for i in tlist:
 #fdf = dddf.join(list_tf)
 
 x = list_tf.loc[:, 'atg':'CGT']
-st.dataframe(x)
+
+
 #決定木
 #with open('yoshidamodel.pkl', 'rb') as f:
 #    ktg = pickle.load(f)
@@ -101,7 +102,7 @@ st.dataframe(x)
 
 #=========================機械学習用===========ここまで
 
-st.dataframe(fdf2.sort_values("No"))
+st.dataframe(x.sort_values("No"))
 
 list2_df = pd.DataFrame( columns=["No","snippet", "rev_compl"])
 
